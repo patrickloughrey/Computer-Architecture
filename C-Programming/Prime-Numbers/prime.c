99d44a3833f0d99151fb1a86ee28e6ea820b3c3a
Patrick Loughrey <PatrickLoughrey@Patricks-MBP-2.home>
2017-03-21 23:17:07 -0400

    Completed program calculating prime number

---
+++ C-Programming/Prime-Numbers/prime.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) 
+{
+    if(argc != 2) {
+      printf("You did not provide the correct number of arguments!\n");
+      exit(0);
+    }
+
+    /* Converts command line argument from character to an integer */
+    int num = atoi(argv[1]);
+    int i = 0;
+
+    for(i = 2; i < num; i++) {
+
+      if((num % i) == 0) {
+        printf("%d is a composite number!\n", num);
+        return 0;
+      }
+
+    }
+
+    printf("%d is a prime number!\n", num);
+    return 0;
+
+}
